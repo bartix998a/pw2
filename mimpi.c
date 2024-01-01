31f1700f373254372f0b519f6ea47f346168c823
bp449309 <b.parapura@students.uw.edu.pl>
2024-01-01 23:56:56 +0100

    MIMPI_barrier implementation

--- mimpi.c
+++ mimpi.c
@@ -259,7 +259,37 @@ MIMPI_Retcode MIMPI_Recv(
     return MIMPI_SUCCESS;
 }
 
-MIMPI_Retcode MIMPI_Barrier(){TODO}
+MIMPI_Retcode MIMPI_Barrier()
+{
+    char signal;
+    if (2 * pid <= world_size)
+    {
+        chrecv(from_leftson, &signal, 1);
+    }
+    /*
+    potentially you can check if you recieved signal for barrier
+    */
+    if (2 * pid + 1 <= world_size)
+    {
+        chrecv(from_rightson, &signal, 1);
+    }
+    signal = MIMPI_BARIER;
+    chsend(to_parent_fd, &signal, 1);
+    chrecv(from_parent_fd, &signal, 1);
+    if (2 * pid <= world_size)
+    {
+        signal = MIMPI_BARIER;
+        chsend(to_leftson, &signal, 1);
+    }
+    /*
+    potentially you can check if you recieved signal for barrier
+    */
+    if (2 * pid + 1 <= world_size)
+    {
+        signal = MIMPI_BARIER;
+        chsend(to_rightson, &signal, 1);
+    }
+}
 
 MIMPI_Retcode MIMPI_Bcast(
     void *data,
